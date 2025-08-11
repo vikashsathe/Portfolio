10096746e2c53fe0d7fed15c5f0861cffe860ea4
vikashsathe <vikashsathe83@gmail.com>
2025-08-11 14:04:43 +0530

    some changes

--- src/assets/project/data/project_data.js
+++ src/assets/project/data/project_data.js
@@ -1,52 +1,69 @@
-import project_07 from '../img/SunMiniMeal.svg'
-import project_01 from '../img/Zarodha.svg'
-import project_02 from '../img/Zarodha.svg'
-import project_03 from '../img/Zarodha.svg'
-import project_04 from '../img/Zarodha.svg'
-import project_05 from '../img/Zarodha.svg'
+import SunMiniMeal from '../img/SunMiniMeal.svg'
+import Zarodha from '../img/Zarodha.svg'
+import MajorProject from '../img/MajorProject.svg'
 
 
 const Projects_Data = [
 
     {
-        id:"07",
-          title: "Smart E-Commerce Website using MERN Stack",
-        img: project_07,
+        id:"01",
+          title: "Sun Mini Meal",
+        img: SunMiniMeal,
+        description: 'I developed a clone of the SUN MINIMEAL website using CodeIgniter in PHP. The project replicates the original site’s clean design, responsive layout, and product details. Features include dynamic content, product showcase, and mobile-friendly UI. It demonstrates my skills in PHP, MVC architecture, and front-end integration.',
+       li: [
+  "Developed with CodeIgniter (MVC framework)",
+  "Responsive and mobile-friendly design",
+  "Dynamic product pages and content management",
+  "Clean UI with original site’s look and feel",
+  "Integrated PHP backend with HTML, CSS, JavaScript",
+  "Focused on performance and scalability"
+]
+
+
+
+    },
+    {
+        id:"02",
+          title: "Zarodha",
+        img: Zarodha,
        description: "Project Description This project is a smart e-commerce website built using the MERN stack  MongoDB, Express.js, React.js, and Node.js. The goal is to create a complete online shopping platform where users can buy products, view details, add items to cart or wishlist, and complete payment.The project has two panels: User Panel for customers Admin Panel for the store manager"
 
 
     },
     {
+        id:"03",
+        title: "MajorProject",
+        img: MajorProject,
+        description: 'I developed a clone of the SUN MINIMEAL website using CodeIgniter in PHP. The project replicates the original site’s clean design, responsive layout, and product details. Features include dynamic content, product showcase, and mobile-friendly UI. It demonstrates my skills in PHP, MVC architecture, and front-end integration.',
+        li: "Developed with CodeIgniter (MVC framework)",
+        li: "Responsive and mobile-friendly design",
+        li: "Responsive and mobile-friendly design",
+        li: 'Dynamic product pages and content management',
+        li: "Clean UI with original site’s look and feel",
+        li: "Integrated PHP backend with HTML, CSS, JavaScript",
+        li: "Focused on performance and scalability"
+    },
+        {
         id:"01",
-          title: "Smart E-Commerce Website using MERN Stack",
-        img: project_01,
+          title: "SunMiniMeal",
+        img: SunMiniMeal,
        description: "Project Description This project is a smart e-commerce website built using the MERN stack  MongoDB, Express.js, React.js, and Node.js. The goal is to create a complete online shopping platform where users can buy products, view details, add items to cart or wishlist, and complete payment.The project has two panels: User Panel for customers Admin Panel for the store manager"
 
 
     },
     {
         id:"02",
-        title: "Project Title 02",
-        img: project_02,
-        description: ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quos cumque corrupti dolorem nobis, laborum quibusdam perferendis ut molestiae voluptas!',
+          title: "Zarodha",
+        img: Zarodha,
+       description: "Project Description This project is a smart e-commerce website built using the MERN stack  MongoDB, Express.js, React.js, and Node.js. The goal is to create a complete online shopping platform where users can buy products, view details, add items to cart or wishlist, and complete payment.The project has two panels: User Panel for customers Admin Panel for the store manager"
+
+
     },
     {
         id:"03",
-        title: "Project Title 03",
-        img: project_03,
-        description:' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quos cumque corrupti dolorem nobis, laborum quibusdam perferendis ut molestiae voluptas!',
-    },
-    {
-        id:"04",
-        title: "Project Title 04",
-        img: project_04,
-        description:' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quos cumque corrupti dolorem nobis, laborum quibusdam perferendis ut molestiae voluptas!',
-    },
-    {
-        id:"05",
-        title: "Project Title 05",
-        img: project_05,
-        description:' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quos cumque corrupti dolorem nobis, laborum quibusdam perferendis ut molestiae voluptas!',
+        title: "MajorProject",
+        img: MajorProject,
+        description: ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quos cumque corrupti dolorem nobis, laborum quibusdam perferendis ut molestiae voluptas!',
     },
 ]
 
--- src/landing_page/projects/ProjectDetials.jsx
+++ src/landing_page/projects/ProjectDetials.jsx
@@ -20,16 +20,24 @@ const ProjectDetials = () => {
   return (
    <div className="row d-flex px-2 px-md-5 justify-content-center align-items-center" style={{paddingTop:"20px"}}>
      <p
-            className="col-10 text-end mt-0 mt-md-5"
+            className="col-md-10 col-12 text-end mt-0 mt-md-5"
             style={{ cursor: "pointer" }}
             onClick={handleBackClick}
           >
-            <i class="ri-arrow-left-circle-fill fs-1"></i>
+            <i class="ri-arrow-left-circle-fill text-muted" style={{fontSize:"50px"}}></i>
           </p>
        <div className="d-flex flex-column flex-md-row justify-content-center align-items-start gap-4">
         <div className="col-md-5 col-12">  <img className="col-md-12 col-12" src={project.img} alt={project.title} style={{cursor:"crosshair"}} /></div>
         <div className="col-md-5 col-12"> <h3 className="col-md-12 mt-3 mb-3 mt-md-0 mb-md-3">{project.title}</h3>
-        <p className="" style={{textAlign:"justify",color:"#55"}}>{project.description}</p></div>
+        <p className="" style={{textAlign:"justify",color:"#55"}}>{project.description}</p>
+         <ul style={{listStyleType:"disc", fontSize:"14px"}} className="col-12 d-flex flex-column gap-2 px-3">
+  {Array.isArray(project.li) && project.li.map((item, index) => (
+    <li key={index} className="">{item}</li>
+  ))}
+</ul>
+
+        </div>
+       
         
 
         
